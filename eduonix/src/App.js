0d6f5a9d79b764815f0ae75f37c4a62b132d3b27
anoopsingh1996 <anoopraghuvansi@gmail.com>
2018-09-20 21:02:01 +0530

    Event_Looping

--- eduonix/src/App.js
+++ eduonix/src/App.js
@@ -5,9 +5,19 @@ import NameComponent from './components/NameComponent';
 
 class App extends Component {
   render() {
+    var word = "Hello";
+    var array = ['Welcome','to','my','course'];
+
+    var arrayToRender = [];
+    array.forEach(function(word) {
+      arrayToRender.push(
+        <p>{word}</p>
+      )
+    });
     return (
       <div>
-        <p>Boom Boom!!</p>
+        <p>Hello</p>
+        {arrayToRender}
         <NameComponent />
       </div>
     );
